40a53225ba45c7210ca3510fc7d7461c8e0ddce3
Christopher Nielsen <c.consult.christopher@gmail.com>
2020-03-19 11:11:34 +0100

    Initialized

---
+++ DriverAndMsgTest_v1/main.c
@@ -0,0 +1,18 @@
+/*
+ * DriverAndMsgTest_v1.c
+ *
+ * Created: 19-Mar-20 11:08:21 AM
+ * Author : teamv
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
